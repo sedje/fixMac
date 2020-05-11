5183ad439e1f3f217b2a9e3cf174498247336111
Anko van der Ziel <azi@AZI-MBP-NW.local>
2020-05-11 11:56:18 +0200

    update fixVMWare to include all required files

--- fixVMWarepermissions.sh
+++ fixVMWarepermissions.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
+
+FILES="/Applications/VMware\ Fusion.app/Contents/Library/LaunchServices/*Helper
+/Library/PrivilegedHelperTools/com.vmware.VMMonHelper
+/Library/PrivilegedHelperTools/com.vmware.KextControlHelper"
+
+for FILE in $FILES 
+do
+        if test -f "$FILE"; then
+                sudo xattr -rd com.apple.quarantine $FILE
+        fi
+done
+
 sudo xattr -rd com.apple.quarantine /Applications/VMware\ Fusion.app/Contents/Library/LaunchServices/*Helper
+sudo xattr -d -r com.apple.quarantine /Library/PrivilegedHelperTools/com.vmware.VMMonHelper
+sudo xattr -d -r com.apple.quarantine /Library/PrivilegedHelperTools/com.vmware.KextControlHelper
